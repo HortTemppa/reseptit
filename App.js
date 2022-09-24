a743dec3904373cfbb817c1d01f0b255f716001b
Teemu Laine <teemu.laine@myy.haaga-helia.fi>
2022-09-24 17:12:22 +0300

    Created basic structure for app

--- App.js
+++ App.js
@@ -1,12 +1,24 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import { Button, FlatList, StyleSheet, Text, TextInput, View } from 'react-native';
 
 export default function App() {
+const [searchWord, setSearchWord] =  useState()
+const [recipies, setRecipies] = useState([])
+
+const getRecipies = async () => {
+ const response = await fetch(`https://www.themealdb.com/api/json/v1/1/filter.php?i=tomato̧https://www.themealdb.com/api/json/v1/1/filter.php?i=${searchWord}`)
+ const json = await response.json();
+
+ console.log(json.body)
+}
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+      <TextInput onChange={(e) => setSearchword(e.target.value)}/>    
+      <Button onPress={getRecipies()}>Search</Button>
+    <StatusBar style="auto" />
+     </View>
   );
 }
 
